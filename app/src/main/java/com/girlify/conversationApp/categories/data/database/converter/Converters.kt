9aeac59d4ad3d028ba05db0fb95c56274ff26beb
Noelia Carosella <72532638+honeybadger2788@users.noreply.github.com>
2023-04-19 17:43:05 -0300

    fix: change converter delimiter (#9)

--- app/src/main/java/com/girlify/conversationApp/categories/data/database/converter/Converters.kt
+++ app/src/main/java/com/girlify/conversationApp/categories/data/database/converter/Converters.kt
@@ -5,11 +5,11 @@ import androidx.room.TypeConverter
 class Converters {
     @TypeConverter
     fun fromString(value: String): List<String> {
-        return value.split(",")
+        return value.split(";")
     }
 
     @TypeConverter
     fun fromList(list: List<String>): String {
-        return list.joinToString(",")
+        return list.joinToString(";")
     }
 }
