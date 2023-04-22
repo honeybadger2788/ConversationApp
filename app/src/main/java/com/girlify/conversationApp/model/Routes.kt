cbc968a92403f316b82975d6505c448e3c731359
Noelia Carosella <72532638+honeybadger2788@users.noreply.github.com>
2023-04-22 13:15:52 -0300

    feature: splash screen implemented (#14)

--- app/src/main/java/com/girlify/conversationApp/model/Routes.kt
+++ app/src/main/java/com/girlify/conversationApp/model/Routes.kt
@@ -1,8 +1,7 @@
 package com.girlify.conversationApp.model
 
-import com.girlify.conversationApp.categories.ui.categoriesScreen.model.CategoryModel
-
 sealed class Routes(val route: String){
+    object SplashScreen:Routes("splashScreen")
     object Categories:Routes("categories")
     object Questions:Routes("questions/{categoryId}"){
         fun createRoute(categoryId: String) = "questions/$categoryId"
