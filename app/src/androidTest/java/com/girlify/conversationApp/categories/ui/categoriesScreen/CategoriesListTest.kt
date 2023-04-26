0b0c8f30f8715d62a5e88aa08c8c303b47d9dcfe
honeybadger2788 <noe.carosella@gmail.com>
2023-04-26 11:36:37 -0300

    wip: categories list test

---
+++ app/src/androidTest/java/com/girlify/conversationApp/categories/ui/categoriesScreen/CategoriesListTest.kt
@@ -0,0 +1,17 @@
+package com.girlify.conversationApp.categories.ui.categoriesScreen
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class CategoriesListTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testUi(){
+        composeTestRule.setContent { 
+        }
+    }
+}
